fd35cc8fdfb808108c246af3dab7190ceb078c82
saadhussain31 <saadnasir31@gmail.com>
2023-04-26 03:59:10 -0400

    Add comments for clarity

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ namespace fs = std::filesystem;
 [[noreturn]] void printUsageAndExit(int status= EXIT_SUCCESS) {
     std::cout << "Usage: twin-seeker [DIR]..\n";
     std::cout << " Detects duplicated files in the given directory(s) recursively.\n";
-    exit(status);
+    exit(status); //exit with a non-zero status code to signal that an error occurred.
 }
 
 int main(int argc, char* argv[]) {
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     
     for(int i = 1; i < argc; ++i) {
         fs::path path{argv[i]};
-        if(!fs::is_directory(path)) {
+        if(!fs::is_directory(path)) { //checks if 'path' represents a valid directory.
             std::cerr << argv[i] << " is not a directory.\n";
             printUsageAndExit(EXIT_FAILURE);
 
