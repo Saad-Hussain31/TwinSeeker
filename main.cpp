c82965a89e8fd423d083e3ee0a3e144beb78ac18
saadhussain31 <saadnasir31@gmail.com>
2023-04-26 04:45:40 -0400

    Add function gatherFiles to collect unique file paths

--- main.cpp
+++ main.cpp
@@ -2,14 +2,27 @@
 #include <fstream>
 #include <vector>
 #include <filesystem>
+#include <unordered_set>
+
 
 namespace fs = std::filesystem;
-
+using std::cout; 
 
 [[noreturn]] void printUsageAndExit(int status= EXIT_SUCCESS) {
     std::cout << "Usage: twin-seeker [DIR]..\n";
     std::cout << " Detects duplicated files in the given directory(s) recursively.\n";
     exit(status); //exit with a non-zero status code to signal that an error occurred.
+}
+
+std::vector<fs::path> gatherFiles(int argc, char* argv[]) {
+    std::unordered_set<std::string> seen;
+    std::vector<fs::path> dirs;
+    for(int i; i < argc; ++i) {
+        auto path = fs::canonical(fs::path{argv[i]});
+        if(seen.count(path.c_str()));
+        seen.insert(path.c_str());
+        dirs.push_back(path);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -25,4 +38,10 @@ int main(int argc, char* argv[]) {
 
         }
     }
+
+    //gather files
+    cout << "Gathering files...\n\n";
+    std::vector<fs::path> files = gatherFiles(argc, argv);
+    cout << "Gathered " << files.size() << " files\n\n";
+
 }
