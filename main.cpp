1273dc58d012b720fd001a3253ae18d1a34ae236
saadhussain31 <saadnasir31@gmail.com>
2023-04-26 09:26:45 -0400

    Add hashing logic for empty files only

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <vector>
 #include <filesystem>
 #include <unordered_set>
+#include <unordered_map>
 
 
 namespace fs = std::filesystem;
 using std::cout; 
+
+std::string sha256(const std::vector<char>& input);
 
 [[noreturn]] void printUsageAndExit(int status= EXIT_SUCCESS) {
     std::cout << "Usage: twin-seeker [DIR]..\n";
@@ -15,17 +18,17 @@ using std::cout;
 }
 
 std::vector<fs::path> gatherFiles(int argc, char* argv[]) {
-    std::unordered_set<std::string> seen;
-    std::vector<fs::path> dirs; //stack
-    for(int i; i < argc; ++i) {
-        auto path = fs::canonical(fs::path{argv[i]});
+    std::unordered_set<std::string> seen; //to keep track of the directories already processed 
+    std::vector<fs::path> dirs; //stack for processing directories
+    for(int i = 1; i < argc; ++i) {
+        auto path = fs::canonical(fs::path{argv[i]}); //removes symlinks, emojis etc
         if(seen.count(path.c_str()) >= 0) continue;
         seen.insert(path.c_str());
         dirs.push_back(path);
     }
 
     //walking the directories
-    std::vector<fs::path> files;
+    std::vector<fs::path> files; //to store the file paths found during the traversal
     while(!dirs.empty()) {
         fs::path dir = dirs.back();
         dirs.pop_back();
@@ -43,7 +46,7 @@ std::vector<fs::path> gatherFiles(int argc, char* argv[]) {
             }
         }
     }
-    return files;
+    return files; //canonical paths of all files in the input directories and their subdirectories
 }
 
 int main(int argc, char* argv[]) {
@@ -59,9 +62,27 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    //gather files.
+    //gather files
     cout << "Gathering files...\n\n";
     std::vector<fs::path> files = gatherFiles(argc, argv);
     cout << "Gathered " << files.size() << " files\n\n";
+
+    //calculate hashes
+    std::unordered_map<std::string, std::vector<fs::path>> visitedFiles; //key is hash and val is file vector. single vec if no dups
+
+    for(auto& file : files) {
+        uintmax_t size = fs::file_size(file);
+        if(size == 0) {
+            std::vector<char> empty;
+            auto hash = sha256(empty);
+            if(visitedFiles.count(hash) == 0) {
+                visitedFiles[hash] = std::vector{file};
+                continue;
+            }
+            visitedFiles[hash].push_back(file); //adding for duplicate
+            continue;
+        }
+    }
+
 
 }
