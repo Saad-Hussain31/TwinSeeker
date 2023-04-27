a92a2c87d387efdd05ffc29495115744c89aa0c4
saadhussain31 <saadnasir31@gmail.com>
2023-04-27 03:09:23 -0400

    File handling and print results

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 namespace fs = std::filesystem;
 using std::cout; 
 
-std::string sha256(const std::vector<char>& input);
+std::string sha256(const std::vector<char>& input) {
+    return "sha256";
+} //hashing function signature only
 
 [[noreturn]] void printUsageAndExit(int status= EXIT_SUCCESS) {
     std::cout << "Usage: twin-seeker [DIR]..\n";
@@ -22,7 +24,7 @@ std::vector<fs::path> gatherFiles(int argc, char* argv[]) {
     std::vector<fs::path> dirs; //stack for processing directories
     for(int i = 1; i < argc; ++i) {
         auto path = fs::canonical(fs::path{argv[i]}); //removes symlinks, emojis etc
-        if(seen.count(path.c_str()) >= 0) continue;
+        if(seen.count(path.c_str()) != 0) continue;
         seen.insert(path.c_str());
         dirs.push_back(path);
     }
@@ -82,7 +84,30 @@ int main(int argc, char* argv[]) {
             visitedFiles[hash].push_back(file); //adding for duplicate
             continue;
         }
+        std::ifstream ifs(file, std::ios::binary);
+        if(!ifs) {
+            std::cerr << "Could not open file: " << file.c_str() << std::endl;
+            continue;
+        }
+        std::vector<char> bytes(size);
+		ifs.read(bytes.data(), size);
+		auto hash = sha256(bytes);
+		if (visitedFiles.count(hash) == 0) {
+			visitedFiles[hash] = std::vector{file};
+			continue;
+		}
+		visitedFiles[hash].push_back(file);
     }
+    // Print results
+	cout << "Duplicates:\n";
+	for (auto& f : visitedFiles) {
+		if (f.second.size() == 1) continue;
+		cout << "Hash: " << f.first << "\n";
+		for (size_t i = 0; i < f.second.size(); ++i) {
+			cout << f.second[i] << "\n";
+		}
+		cout << "---\n";
+	}
 
 
 }
