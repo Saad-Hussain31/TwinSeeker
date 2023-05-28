97cfdfc8c696319c1e5cf6ec65f97960be50e4d2
saadhussain31 <saadnasir31@gmail.com>
2023-05-28 14:55:27 -0400

    Use intel's special hardware instructions for sha256

--- src/main.cpp
+++ src/main.cpp
@@ -2,125 +2,121 @@
 #include <fstream>
 #include <vector>
 #include <filesystem>
-#include <unordered_set>
+#include <string>
 #include <unordered_map>
-#include "sha256.hpp"
+#include <unordered_set>
+#include <cstring>
 
+// #include <sha256.h> // Let's treat this as a black box
+#include "../src/flo-shani.h"
+
+using namespace std;
 namespace fs = std::filesystem;
-using std::cout;
 
 [[noreturn]] void printUsageAndExit(int status = EXIT_SUCCESS) {
-    std::cout << "Usage: twin-seeker [DIR]..\n";
-    std::cout << " Detects duplicated files in the given directory(s) recursively.\n";
+    cout << "Usage: dedup [DIR]..\n";
+    cout << "  Detects duplicated files in the given directory(s) recursively.\n";
     exit(status);
 }
 
-std::vector<fs::path> gatherFilesRecursive(const fs::path& directory, std::unordered_set<std::string>& seen) {
-    std::vector<fs::path> files;
-
-    for (const auto& entry : fs::recursive_directory_iterator(directory)) {
-        const fs::path& path = entry.path();
-        if (fs::is_directory(path)) {
-            fs::path canonicalPath = fs::canonical(path);
-            if (seen.count(canonicalPath.c_str()) != 0) {
+vector<fs::path> gatherFiles(int argc, char* argv[]) {
+    unordered_set<string> seen;
+    vector<fs::path> dirs; // We'll treat this as a stack
+    for (int i = 1; i < argc; ++i) {
+        auto p = fs::canonical(fs::path{argv[i]}); // This can throw exception, which is fine
+        if (seen.count(p.c_str()) != 0) continue;
+        seen.insert(p.c_str());
+        dirs.push_back(p);
+    }
+    // Walk the directories
+    vector<fs::path> files;
+    while (!dirs.empty()) {
+        fs::path d = dirs.back();
+        dirs.pop_back();
+        for (auto it = fs::directory_iterator(d); it != fs::directory_iterator(); ++it) {
+            // Two states we care about: 1, directory 2, regular file
+            if (fs::is_directory(*it)) {
+                fs::path newDir = fs::canonical(*it);
+                if (seen.count(newDir.c_str()) != 0) continue;
+                dirs.push_back(newDir);
+                seen.insert(newDir.c_str());
                 continue;
             }
-            seen.insert(canonicalPath.c_str());
-            auto subFiles = gatherFilesRecursive(canonicalPath, seen);
-            files.insert(files.end(), subFiles.begin(), subFiles.end());
-        }
-        else if (fs::is_regular_file(path)) {
-            files.push_back(path);
+            if (fs::is_regular_file(*it)) {
+                files.push_back(fs::canonical(*it));
+            }
         }
     }
-
     return files;
 }
 
-std::vector<fs::path> gatherFiles(int argc, char* argv[]) {
-    std::unordered_set<std::string> seen;
-    std::vector<fs::path> files;
-
-    for (int i = 1; i < argc; ++i) {
-        fs::path directory{ argv[i] };
-        if (!fs::is_directory(directory)) {
-            std::cerr << argv[i] << " is not a directory.\n";
-            printUsageAndExit(EXIT_FAILURE);
-        }
-
-        fs::path canonicalPath = fs::canonical(directory);
-        if (seen.count(canonicalPath.c_str()) != 0) {
-            continue;
-        }
-        seen.insert(canonicalPath.c_str());
-        auto subFiles = gatherFilesRecursive(canonicalPath, seen);
-        files.insert(files.end(), subFiles.begin(), subFiles.end());
+string digestToStr(unsigned char digest[]) {
+    char buf[65];
+    buf[64] = 0;
+    for (unsigned i = 0; i < 32; ++i) {
+        sprintf(buf + i * 2, "%02x", digest[i]);
     }
-
-    return files;
-}
-
-std::string calculateFileHash(const fs::path& file) {
-    uintmax_t size = fs::file_size(file);
-    if (size == 0) {
-        return "";
-    }
-
-    std::ifstream ifs(file, std::ios::binary);
-    if (!ifs) {
-        std::cerr << "Could not open file: " << file.c_str() << std::endl;
-        return "";
-    }
-
-    std::vector<char> bytes(size);
-    ifs.read(bytes.data(), size);
-    return sha256(bytes);
-}
-
-void findDuplicateFiles(const std::vector<fs::path>& files, std::unordered_map<std::string, std::vector<fs::path>>& visitedFiles) {
-    for (const auto& file : files) {
-        std::string hash = calculateFileHash(file);
-        if (hash.empty()) {
-            visitedFiles[hash].push_back(file);
-            continue;
-        }
-
-        if (visitedFiles.count(hash) == 0) {
-            visitedFiles[hash] = std::vector<fs::path>{file};
-        }
-        else {
-            visitedFiles[hash].push_back(file);
-        }
-    }
-}
-
-void printDuplicates(const std::unordered_map<std::string, std::vector<fs::path>>& visitedFiles) {
-    cout << "Duplicates:\n";
-    for (const auto& f : visitedFiles) {
-        if (f.second.size() == 1) {
-            continue;
-        }
-        cout << "Hash: " << f.first << "\n";
-        for (const auto& file : f.second) {
-            cout << file << "\n";
-        }
-        cout << "---\n";
-    }
+    return string(buf);
 }
 
 int main(int argc, char* argv[]) {
+    // Check arguments
     if (argc < 2) {
         printUsageAndExit();
     }
-
-    std::vector<fs::path> files = gatherFiles(argc, argv);
+    for (int i = 1; i < argc; ++i) {
+        fs::path p{argv[i]};
+        if (!fs::is_directory(p)) {
+            cerr << argv[i] << " is not a directory.\n";
+            printUsageAndExit(EXIT_FAILURE);
+        }
+    }
+    // Gather files
     cout << "Gathering files...\n\n";
+    vector<fs::path> files = gatherFiles(argc, argv);
     cout << "Gathered " << files.size() << " files\n\n";
-
-    std::unordered_map<std::string, std::vector<fs::path>> visitedFiles;
-    findDuplicateFiles(files, visitedFiles);
-
-    printDuplicates(visitedFiles);
-
+    // Calculate hashes
+    cout << "Calculating SHA-256 hashes..\n\n";
+    unordered_map<string, vector<fs::path>> visitedFiles; // Key: SHA-256 hash Value: list of files
+    for (auto& file : files) {
+        uint8_t digest[32];
+        memset(digest, 0, 32);
+        uintmax_t size = fs::file_size(file);
+        if (size == 0) {
+            sha256_update_shani(nullptr, 0, digest);
+            auto hash = digestToStr(digest);
+            if (visitedFiles.count(hash) == 0) {
+                visitedFiles[hash] = vector{file};
+                continue;
+            }
+            visitedFiles[hash].push_back(file);
+            continue;
+        }
+        ifstream ifs(file, ios::binary);
+        if (!ifs) {
+            cerr << "Couldn't open file: " << file.c_str() << std::endl;
+            continue; // Move on
+        }
+        char* bytes = (char *) aligned_alloc(32, size); // 128, 256 bits long registers
+        ifs.read(bytes, size);
+        sha256_update_shani(reinterpret_cast<unsigned char*>(bytes), size, digest);
+        free(bytes);
+        auto hash = digestToStr(digest);
+        if (visitedFiles.count(hash) == 0) {
+            visitedFiles[hash] = vector{file};
+            continue;
+        }
+        visitedFiles[hash].push_back(file);
+    }
+    // Print results
+    cout << "Duplicates:\n";
+    for (auto& f : visitedFiles) {
+        if (f.second.size() == 1) continue;
+        cout << "Hash: " << f.first << "\n";
+        for (size_t i = 0; i < f.second.size(); ++i) {
+            cout << f.second[i] << "\n";
+        }
+        cout << "---\n";
+    }
     return 0;
 }
